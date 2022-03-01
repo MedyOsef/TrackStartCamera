4b708fb9a185b1e0faeb7e761e6d5ea289f2edb9
MedyOsef <porgomedyfaycal@gmail.com>
2022-03-01 19:23:55 +0000

    clear video folder

--- poseEstimation.py
+++ poseEstimation.py
@@ -6,7 +6,7 @@ mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
 
-cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
         ret, frame = cap.read()
